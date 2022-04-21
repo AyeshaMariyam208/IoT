08d84f37cbcae07e41981fd9a911d69673402d8a
AyeshaMariyam208 <83399302+AyeshaMariyam208@users.noreply.github.com>
2022-04-21 21:50:04 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+st.title('IEEE IoT')
+from PIL import Image
+image = Image.open('sunrise.jpg')
+st.image(image, caption='Sunrise by the mountains')
