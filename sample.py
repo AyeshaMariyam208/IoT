be3382ca90c4d304a71069d0371489ba5b409d72
AyeshaMariyam208 <83399302+AyeshaMariyam208@users.noreply.github.com>
2022-04-28 22:41:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,3 +6,13 @@ st.title('IEEE IoT')
 from PIL import Image
 image = Image.open('sunrise.jpg')
 st.image(image, caption='Sunrise by the mountains')
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+  with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
